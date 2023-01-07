51cdde397576e573d2a262570942ace3ab5ed2d0
nomercy25 <91740728+nomercy25@users.noreply.github.com>
2023-01-07 17:35:49 +0200

    Add files via upload

---
+++ js/burger.js
@@ -0,0 +1,8 @@
+$('.mobile div').on('click', () => {
+    $('.mobile div').toggleClass('active');
+    $('.mobile nav').toggleClass('open');
+    $('.mobile nav ul').toggleClass('show');
+});
+for (let a = 1; a <= $(".mobile ul li").length; a++){
+    $(".mobile ul li:nth-child("+ a +")").css("animation-delay", "."+ (a+1) +"s");   
+}
